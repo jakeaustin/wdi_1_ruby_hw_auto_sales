ba8fcc846c21818f3af918bdb7293624724e51ae
Jake Austin <jakeaust07@gmail.com>
2014-06-12 18:36:36 -0400

    incorporate damage into value calculation

--- bin/test_cars.rb
+++ bin/test_cars.rb
@@ -1,10 +1,18 @@
 require_relative '../lib/used_car.rb'
 require 'pry'
+
 testCar = Car.new('Land Rover', 'Range Rover', 2003, 30_000)
+
 testUsedCar = UsedCar.new('Land Rover', 'Range Rover', 2003, 30_000, 10_000)
+
+testDamage = Damage.new("broken mirror", 50)
+
+# damages passed in as array of damage
+testDamagedCar = UsedCar.new('Land Rover', 'Range Rover', 2003, 30_000, 10_000, damages: [testDamage])
 
 
 puts testCar.inspect
 puts testUsedCar.inspect
+puts testDamagedCar.inspect
 
 binding.pry
--- lib/used_car.rb
+++ lib/used_car.rb
@@ -7,14 +7,16 @@ class UsedCar < Car
 
   attr_reader :mileage
 
-  def initialize(make, model, year, msrp, mileage, damages: :none)
+  def initialize(make, model, year, msrp, mileage, damages: nil)
     super(make, model, year, msrp)
     @mileage = mileage
     @damages = damages
   end
 
   def value
-    super - (@mileage / 10_000) * (MILEAGE_DEPRECIATION * @msrp)
+    val = super - (@mileage / 10_000) * (MILEAGE_DEPRECIATION * @msrp)
+    @damages.each { |damage| val -= damage.cost_to_fix } if @damages
+    val
   end
 
 end
