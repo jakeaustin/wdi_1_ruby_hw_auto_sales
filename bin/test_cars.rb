fd236f62e741c92d536981b123707c6dd613a5c2
Jake Austin <jakeaust07@gmail.com>
2014-06-12 18:21:29 -0400

    implement usedCar as subclass, modify value function

--- bin/test_cars.rb
+++ bin/test_cars.rb
@@ -1,7 +1,10 @@
-require_relative '../lib/car.rb'
+require_relative '../lib/used_car.rb'
 require 'pry'
 testCar = Car.new('Land Rover', 'Range Rover', 2003, 30_000)
+testUsedCar = UsedCar.new('Land Rover', 'Range Rover', 2003, 30_000, 10_000)
+
 
 puts testCar.inspect
+puts testUsedCar.inspect
 
 binding.pry
